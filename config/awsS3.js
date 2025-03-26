2dcf97332b6e91a9671b5e6fe7db200fc4229ac9
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-03-26 14:51:28 +0000

    agregado el amazonS3

---
+++ config/awsS3.js
@@ -0,0 +1,43 @@
+const { S3 } = require("@aws-sdk/client-s3");
+const BUCKET = process.env.AWS_BUCKET_NAME;
+class AwsS3 {
+
+  static __self = null;
+  static getInstance() {
+    if (this.__self == null) {
+      this.__self = new AwsS3();
+    }
+    return this.__self;
+  }
+  constructor() {
+    this.s3 = new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    });
+  }
+
+  async uploadFile(file) {
+    const params = {
+      Bucket: BUCKET,
+      Key: file.originalname,
+      Body: file.buffer,
+    };
+    return this.s3.upload(params).promise();
+  }
+
+  async getFile(fileKey) {
+    const params = {
+      Bucket: BUCKET,
+      Key: fileKey,
+    };
+    return this.s3.getObject(params).promise();
+  }
+
+  async deleteFile(fileKey) {
+    const params = {
+      Bucket: BUCKET,
+      Key: fileKey,
+    };
+    return this.s3.deleteObject(params).promise();
+    }
+}
