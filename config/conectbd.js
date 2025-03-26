ae9950f74dec1c43173fa6d0e7c0d04ec11b7a5d
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-03-26 14:12:20 +0000

    agregados las variables de entorno

--- config/conectbd.js
+++ config/conectbd.js
@@ -1,11 +1,10 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'falabelladb.cruw88i8wma8.us-east-2.rds.amazonaws.com',
-  database: 'Falabella',
-  password: 'Pepeelmago123',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
-
 module.exports = pool;
