a1f9fc58ce06c9bc9564340b159102f70dca5501
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-03-31 17:52:30 +0000

    productos agregados

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getProductByObjectId, getProductByCategoryId } = require('../controllers/productsControler');
+const { 
+    getProductByObjectId, 
+    getProductByCategoryId, 
+    getProductsNumberByCategory,
+    getAllMarcaByCategoryId,
+    getCategoryNameByCategoryId,
+    getMinPriceByCategoryId,
+    getMaxPriceByCategoryId
+} = require('../controllers/productsControler');
 
 router.get('/:id', getProductByObjectId);
-router.get('/category/:id', getProductByCategoryId);
+router.get('/number/:id', getProductsNumberByCategory);
+router.get('/minPrice/:id', getMinPriceByCategoryId);
+router.get('/maxPrice/:id', getMaxPriceByCategoryId);
+router.post('/category/:id', getProductByCategoryId);
+router.get('/categoryName/:id', getCategoryNameByCategoryId);
+router.get('/tradeMark/:id', getAllMarcaByCategoryId);
 
 module.exports = router;
